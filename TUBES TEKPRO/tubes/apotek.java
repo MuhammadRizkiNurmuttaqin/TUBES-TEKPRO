246de22cb7dd3433c373ee6e10f5e68e6cc998f6
MuhammadRizkiNurmuttaqin <123435100+MuhammadRizkiNurmuttaqin@users.noreply.github.com>
2023-03-28 13:06:16 +0700

    Add files via upload

---
+++ TUBES TEKPRO/tubes/apotek.java
@@ -0,0 +1,13 @@
+package tubes;
+
+
+public class apotek{
+    private int NoCustomer;
+    public apotek(int noCustomer, int jumlahuang) {
+        NoCustomer = noCustomer;
+        this.jumlahuang = jumlahuang;
+    }
+    private int jumlahuang;
+
+    
+}
