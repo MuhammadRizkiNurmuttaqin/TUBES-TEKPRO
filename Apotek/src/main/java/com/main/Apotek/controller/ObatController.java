3793a2413b7852f72a7478bd2c4cb63772d80f92
hanifzndn <mohdhanif1610@gmail.com>
2023-05-14 15:29:46 +0700

    new

--- Apotek/src/main/java/com/main/Apotek/controller/ObatController.java
+++ Apotek/src/main/java/com/main/Apotek/controller/ObatController.java
@@ -16,7 +16,7 @@ import com.main.Apotek.service.ObatService;
 
 
 @Controller
-@RequestMapping
+@RequestMapping("home")
 public class ObatController {
 	
 	private final ObatService obatService;
@@ -25,63 +25,58 @@ public class ObatController {
 	public ObatController(ObatService obatService) {
 		this.obatService = obatService;
 	}
-
-	@GetMapping
-	public String tampilkanHome(Model model) {
 	
-		return "home";
-	}
-	
-	@PostMapping("/addObat")
-	public String addObat(@RequestParam("name") String name, @RequestParam("keterangan") String keterangan, @RequestParam("category") String category, @RequestParam("amount") String amount, @RequestParam("harga") String harga) {
-		
-		BigDecimal Harga = new BigDecimal(harga);
-		Obat obat = new Obat(name, keterangan, ObatCategory.valueOf(category.toUpperCase()), Integer.parseInt(amount), Harga);
-		obatService.addObat(obat);
-		return "daftarObat";
-	}
-
-	@PostMapping("/deleteObat")
-	public String delObat(@RequestParam("id") String id){
-		obatService.deleteObat(id);
-		return "daftarObat";
-	}
-
-	@PostMapping("/editObat")
-	public String putObat(@RequestParam("id") String id, @RequestParam("name") String name, @RequestParam("keterangan") String keterangan, @RequestParam("category") String category, @RequestParam("amount") String amount, @RequestParam("harga") String harga){
-		Obat obat = obatService.getPaketById(id);
-		BigDecimal Harga = new BigDecimal(harga);
-		obat.setObatName(name);
-		obat.setObatKeterangan(keterangan);
-		obat.setObatCategory(ObatCategory.valueOf(category.toUpperCase()));
-		obat.setObatAmount(Integer.parseInt(amount));
-		obat.setObatHarga(Harga);
-
-		obatService.updateObat(obat);
-
-		return "daftarObat";
-	}
-	
-
-	@GetMapping("/tampilkanDaftarObat")
+	@GetMapping("/daftar-obat")
 	public String tampilkanDaftarObat(Model model) {
 		List<Obat> obat = obatService.getAllObat();
 		model.addAttribute("listobat", obat);
 	
-		return "daftarObat";
+		return "daftar-obat";
 	}
+	
+	// @PostMapping("/addObat")
+	// public String addObat(@RequestParam("name") String name, @RequestParam("keterangan") String keterangan, @RequestParam("category") String category, @RequestParam("amount") String amount, @RequestParam("harga") String harga) {
+		
+	// 	BigDecimal Harga = new BigDecimal(harga);
+	// 	Obat obat = new Obat(name, keterangan, ObatCategory.valueOf(category.toUpperCase()), Integer.parseInt(amount), Harga);
+	// 	obatService.addObat(obat);
+	// 	return "daftarObat";
+	// }
 
-	@GetMapping("/tampilkanAddObat")
-	public String tampilkanAddObat(Model model) {
+	// @PostMapping("/deleteObat")
+	// public String delObat(@RequestParam("id") String id){
+	// 	obatService.deleteObat(id);
+	// 	return "daftarObat";
+	// }
 
-		return "addObat";
-	}
+	// @PostMapping("/editObat")
+	// public String putObat(@RequestParam("id") String id, @RequestParam("name") String name, @RequestParam("keterangan") String keterangan, @RequestParam("category") String category, @RequestParam("amount") String amount, @RequestParam("harga") String harga){
+	// 	Obat obat = obatService.getPaketById(id);
+	// 	BigDecimal Harga = new BigDecimal(harga);
+	// 	obat.setObatName(name);
+	// 	obat.setObatKeterangan(keterangan);
+	// 	obat.setObatCategory(ObatCategory.valueOf(category.toUpperCase()));
+	// 	obat.setObatAmount(Integer.parseInt(amount));
+	// 	obat.setObatHarga(Harga);
 
-	@GetMapping("/tampilkanEditObat")
-	public String tampilkanEditObat(Model model) {
+	// 	obatService.updateObat(obat);
 
-		return "editObat";
-	}
+	// 	return "daftarObat";
+	// }
+	
+
+
+	// @GetMapping("/tampilkanAddObat")
+	// public String tampilkanAddObat(Model model) {
+
+	// 	return "addObat";
+	// }
+
+	// @GetMapping("/tampilkanEditObat")
+	// public String tampilkanEditObat(Model model) {
+
+	// 	return "editObat";
+	// }
 
 	}
 
